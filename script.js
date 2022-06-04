8fcce1eab00abfbb1f9781a36e35805627e3fd6a
Olamide Okunola <97003277+LammyG@users.noreply.github.com>
2022-06-04 01:03:02 +0000

    i added some codes to the index.html and script.js

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
  function guessNumber() {
-  var randomNum = Math.floor(Math.random() *3);
+  var randomNum = Math.floor(Math.random() * 2 + 1);
  
 
   // output to the console for debugging
   console.log(randomNum);
   // declare a varible
   var guess;
-  guess = prompt("Please enter a number between 1 and 2");
+  guess = prompt("Please enter a number between 1 to 2");
   console.log("Your guess is: " + guess);
 
-  if (guess < random) {
+  if (guess < randomNum) {
       window.alert("your guess is too low");
   } else if (guess > randomNum) {
       window.alert("Your guess is too high");
   } else if (guess == randomNum) {
-      window.alert("You guessed it. You win!!!");
+      window.alert("You guessed it right. unto the next stage. You win a point!!!");
   }
-}
+};
+
+// stage 2
+var randomNum = Math.floor(Math.random() * 2 + 1);
+ 
+
+  // output to the console for debugging
+  console.log(randomNum);
+  // declare a varible
+  var guess;
+  guess = prompt("Please enter a number between 1 to 3");
+  console.log("Your guess is: " + guess);
+
+  if (guess < randomNum) {
+      window.alert("your guess is too low");
+  } else if (guess > randomNum) {
+      window.alert("Your guess is too high");
+  } else if (guess == randomNum) {
+      window.alert("You guessed it right. You win a point!!!");
+  }
   
